1f4d3637117063f1f3f068f3e6117f61d70b0c93
Thejus Paul <thejuspaul@pm.me>
2021-04-02 21:53:05 +0530

    refactor: combined duplicate codes into a function

--- content_script.js
+++ content_script.js
@@ -11,6 +11,13 @@
 	let style = '<style>*{box-sizing:border-box;margin:0;padding:0;border:0;outline:none}body{display:flex;justify-content:center;align-items:center;flex-direction:column;height:90vh;background-color:#fce38a}.login-box{display:flex;justify-content:center;align-items:center;flex-direction:column;padding:1rem;width:40vh;border-radius:10px}.login-box form{width:inherit}.login-box input{display:flex;justify-content:center;align-items:center;flex-direction:column;width:inherit;font-size:1rem;padding:1rem;border-radius:10px;font-weight:bold;text-align:center;letter-spacing:2px;background-color:#111;color:#fce38a;margin-top:1rem}.selection{display:flex;justify-content:center;align-items:center;flex-direction:row}.selection a{text-decoration:none}@media screen and (max-width: 800px){.selection{display:flex;justify-content:center;align-items:center;flex-direction:column;margin-bottom:1rem;margin-right:0rem}}.selection .selection__attendance,.selection .selection__marks{display:flex;justify-content:center;align-items:center;flex-direction:row;letter-spacing:2px;font-family:"Roboto Slab",sans-serif;font-weight:bold;padding:1rem;border:4px solid #111;margin-right:2.5rem;color:#111}@media screen and (max-width: 800px){.selection .selection__attendance,.selection .selection__marks{display:flex;justify-content:center;align-items:center;flex-direction:column;margin-bottom:1rem;margin-right:0rem}}.selection .selection__attendance:hover,.selection .selection__marks:hover{background-color:#fbd758}</style>';
 	head.insertAdjacentHTML('afterbegin',(metadata+style));
 
+	// To remove the pre-existing tags
+	function removeExistingTags() {
+		document.getElementsByTagName('html')[0].children.item(1).remove();
+		document.getElementsByTagName('html')[0].children.item(0).remove();
+		console.log("Removed Existing Tags!");
+	}
+
 	// To generate and return the leave data
 	function generateLeaveData(tableElement) {
 		let table = tableElement[3];
@@ -41,15 +48,10 @@
 		let attendanceTable = document.getElementsByTagName('table');
 		let leaveData = generateLeaveData(attendanceTable);
 
-
-		document.getElementsByTagName('html')[0].children.item(1).remove();
-		document.getElementsByTagName('html')[0].children.item(0).remove();
-		console.log("Removed Existing Tags!");
+		removeExistingTags();
 	}
 	else if(window.location.href == "https://www.rajagiritech.ac.in/stud/KTU/Parent/Home.asp") {
-		document.getElementsByTagName('html')[0].children.item(1).remove();
-		document.getElementsByTagName('html')[0].children.item(0).remove();
-		console.log("Removed Existing Tags!");
+		removeExistingTags();
 
 		document.getElementsByTagName('html')[0].append(head);
 
@@ -59,9 +61,7 @@
 		document.getElementsByTagName('html')[0].append(body);
 	}
 	else if(window.location.href == "https://www.rajagiritech.ac.in/stud/parent/") {
-		document.getElementsByTagName('html')[0].children.item(1).remove();
-		document.getElementsByTagName('html')[0].children.item(0).remove();
-		console.log("Removed Existing Tags!");
+		removeExistingTags();
 
 		document.getElementsByTagName('html')[0].append(head);
 
