bfba1bd544e1af157f8e042f56a668bda4cc3d33
Thejus Paul <thejuspaul@pm.me>
2021-04-02 21:06:05 +0530

    feat: generates the leave data

--- content_script.js
+++ content_script.js
@@ -11,7 +11,42 @@
 	let style = '<style>*{box-sizing:border-box;margin:0;padding:0;border:0;outline:none}body{display:flex;justify-content:center;align-items:center;flex-direction:column;height:90vh;background-color:#fce38a}.login-box{display:flex;justify-content:center;align-items:center;flex-direction:column;padding:1rem;width:40vh;border-radius:10px}.login-box form{width:inherit}.login-box input{display:flex;justify-content:center;align-items:center;flex-direction:column;width:inherit;font-size:1rem;padding:1rem;border-radius:10px;font-weight:bold;text-align:center;letter-spacing:2px;background-color:#111;color:#fce38a;margin-top:1rem}.selection{display:flex;justify-content:center;align-items:center;flex-direction:row}.selection a{text-decoration:none}@media screen and (max-width: 800px){.selection{display:flex;justify-content:center;align-items:center;flex-direction:column;margin-bottom:1rem;margin-right:0rem}}.selection .selection__attendance,.selection .selection__marks{display:flex;justify-content:center;align-items:center;flex-direction:row;letter-spacing:2px;font-family:"Roboto Slab",sans-serif;font-weight:bold;padding:1rem;border:4px solid #111;margin-right:2.5rem;color:#111}@media screen and (max-width: 800px){.selection .selection__attendance,.selection .selection__marks{display:flex;justify-content:center;align-items:center;flex-direction:column;margin-bottom:1rem;margin-right:0rem}}.selection .selection__attendance:hover,.selection .selection__marks:hover{background-color:#fbd758}</style>';
 	head.insertAdjacentHTML('afterbegin',(metadata+style));
 
-	if(window.location.href == "https://www.rajagiritech.ac.in/stud/KTU/Parent/Home.asp"){
+	// To generate and return the leave data
+	function generateLeaveData(tableElement) {
+		let table = tableElement[3];
+		let tdElements = table.getElementsByTagName('td');
+		let subjects = {
+			"IT402": "CCS",
+			"IT404": "DA",
+			"CS468": "CC",
+			"MA484": "OR"
+		};
+		let leaveData = {};
+		let date = "";
+		for(let i = 8, length = tdElements.length; i < length; i++) {
+			let item = tdElements[i].textContent;
+			if(item != "") {
+				if(item in subjects) {
+					leaveData[date].push(subjects[item.textContent]);
+				} else {
+					date = item.textContent;
+					leaveData[date] = [];
+				}
+			}
+		}
+		return(leaveData);
+	}
+
+	if(window.location.href == "https://www.rajagiritech.ac.in/stud/KTU/Parent/Leave.asp?code=2021S8IT") {
+		let attendanceTable = document.getElementsByTagName('table');
+		let leaveData = generateLeaveData(attendanceTable);
+
+
+		document.getElementsByTagName('html')[0].children.item(1).remove();
+		document.getElementsByTagName('html')[0].children.item(0).remove();
+		console.log("Removed Existing Tags!");
+	}
+	else if(window.location.href == "https://www.rajagiritech.ac.in/stud/KTU/Parent/Home.asp") {
 		document.getElementsByTagName('html')[0].children.item(1).remove();
 		document.getElementsByTagName('html')[0].children.item(0).remove();
 		console.log("Removed Existing Tags!");
@@ -23,7 +58,7 @@
 		body.insertAdjacentHTML('afterbegin','<div class="selection"><a href="https://www.rajagiritech.ac.in/stud/KTU/Parent/Leave.asp?code=2021S8IT"><div class="selection__attendance"><img src="https://img.icons8.com/dotty/128/attendance-mark.png" alt="Attendance icon" loading="lazy"><span>ATTENDANCE</span></div></a><a href="https://www.rajagiritech.ac.in/stud/KTU/Parent/Mark.asp"><div class="selection__marks"><img src="https://img.icons8.com/dotty/128/grades.png" alt="Grades icon" loading="lazy"><span>MARKS</span></div></a></div>');
 		document.getElementsByTagName('html')[0].append(body);
 	}
-	else if(window.location.href == "https://www.rajagiritech.ac.in/stud/parent/"){
+	else if(window.location.href == "https://www.rajagiritech.ac.in/stud/parent/") {
 		document.getElementsByTagName('html')[0].children.item(1).remove();
 		document.getElementsByTagName('html')[0].children.item(0).remove();
 		console.log("Removed Existing Tags!");
