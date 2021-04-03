740161c29fce0663dc880a3eb31ffef95b480b16
Thejus Paul <thejuspaul@pm.me>
2021-04-03 14:55:58 +0530

    feat: display attendance details

--- content_script.js
+++ content_script.js
@@ -8,7 +8,7 @@
 	// To generate head tag and append its elements
 	let head = document.createElement("head");
 	let metadata = '<meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1"><title>RSMS RWD</title>';
-	let style = '<style>*{box-sizing:border-box;margin:0;padding:0;border:0;outline:none}body{display:flex;justify-content:center;align-items:center;flex-direction:column;height:90vh;background-color:#fce38a}.login-box{display:flex;justify-content:center;align-items:center;flex-direction:column;padding:1rem;width:40vh;border-radius:10px}.login-box form{width:inherit}.login-box input{display:flex;justify-content:center;align-items:center;flex-direction:column;width:inherit;font-size:1rem;padding:1rem;border-radius:10px;font-weight:bold;text-align:center;letter-spacing:2px;background-color:#111;color:#fce38a;margin-top:1rem}.selection{display:flex;justify-content:center;align-items:center;flex-direction:row}.selection a{text-decoration:none}@media screen and (max-width: 800px){.selection{display:flex;justify-content:center;align-items:center;flex-direction:column;margin-bottom:1rem;margin-right:0rem}}.selection .selection__attendance,.selection .selection__marks{display:flex;justify-content:center;align-items:center;flex-direction:row;letter-spacing:2px;font-family:"Roboto Slab",sans-serif;font-weight:bold;padding:1rem;border:4px solid #111;margin-right:2.5rem;color:#111}@media screen and (max-width: 800px){.selection .selection__attendance,.selection .selection__marks{display:flex;justify-content:center;align-items:center;flex-direction:column;margin-bottom:1rem;margin-right:0rem}}.selection .selection__attendance:hover,.selection .selection__marks:hover{background-color:#fbd758}</style>';
+	let style = '<style>*{box-sizing:border-box;margin:0;padding:0;border:0;outline:none}body{display:flex;justify-content:center;align-items:center;flex-direction:column;height:90vh;background-color:#fce38a}.login-box{display:flex;justify-content:center;align-items:center;flex-direction:column;padding:1rem;width:40vh;border-radius:10px}.login-box form{width:inherit}.login-box input{display:flex;justify-content:center;align-items:center;flex-direction:column;width:inherit;font-size:1rem;padding:1rem;border-radius:10px;font-weight:bold;text-align:center;letter-spacing:2px;background-color:#111;color:#fce38a;margin-top:1rem}.selection{display:flex;justify-content:center;align-items:center;flex-direction:row}.selection a{text-decoration:none}@media screen and (max-width: 800px){.selection{display:flex;justify-content:center;align-items:center;flex-direction:column;margin-bottom:1rem;margin-right:0rem}}.selection .selection__attendance,.selection .selection__marks{display:flex;justify-content:center;align-items:center;flex-direction:row;letter-spacing:2px;font-family:"Roboto Slab",sans-serif;font-weight:bold;padding:1rem;border:4px solid #111;margin-right:2.5rem;color:#111}@media screen and (max-width: 800px){.selection .selection__attendance,.selection .selection__marks{display:flex;justify-content:center;align-items:center;flex-direction:column;margin-bottom:1rem;margin-right:0rem}}.selection .selection__attendance:hover,.selection .selection__marks:hover{background-color:#fbd758}.attendance{display:flex;justify-content:center;align-items:center;flex-direction:row;flex-wrap:wrap}.subjects{display:flex;justify-content:center;align-items:center;flex-direction:row;flex-wrap:wrap;padding:1rem;margin:.5rem !important;border:4px solid #111}@media screen and (max-width: 800px){.subjects{display:flex;justify-content:center;align-items:center;flex-direction:column;margin-bottom:1rem;margin-right:0rem}}.subjects span{font-family:"Roboto Slab",-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans","Helvetica Neue",sans-serif;margin-right:.5rem;font-size:1.25rem}.subjects .subjects__name{font-weight:bold}</style>';
 	head.insertAdjacentHTML('afterbegin',(metadata+style));
 
 	// To remove the pre-existing tags
@@ -60,27 +60,52 @@
 		return(subjectwiseLeaveCount);
 	}
 
-	// To calculate the current attendance percentage and bunkable data
+	// To calculate and display the current attendance percentage and bunkable data
 	function calcAttendanceDetails(leaveCount) {
 		let totalClasses = {"CCS":6,"DA":6,"CC":6,"OR":6};
+
+		let attendanceElement = document.getElementsByClassName("attendance")[0];
+
 		for(let subject in totalClasses) {
-			let percentage = (((totalClasses[subject] - leaveCount[subject]) / totalClasses[subject]) * 100).toFixed(3);
-			let bunkable = Math.floor(((0.25*totalClasses[subject]) - leaveCount[subject])/0.75)
-			console.log(subject,"Bunkable:",bunkable,"Percentage:",percentage);
+			let subjects = document.createElement("div");
+			subjects.setAttribute("class","subjects");
+
+			let subjectName = document.createElement("span");
+			subjectName.setAttribute("class","subjects__name");
+			subjectName.textContent = subject;
+
+			let subjectPercentage = document.createElement("span");
+			let percentage = (((totalClasses[subject] - leaveCount[subject]) / totalClasses[subject]) * 100).toFixed(2);
+			percentage = percentage == 100.00 ? "100.0%" : (percentage + "%");
+			subjectPercentage.textContent = percentage;
+
+			let subjectBunkable = document.createElement("span");
+			let bunkable = Math.floor(((0.25*totalClasses[subject]) - leaveCount[subject])/0.75);
+			subjectBunkable.textContent = bunkable;
+			
+			subjects.appendChild(subjectName);
+			subjects.appendChild(subjectPercentage);
+			subjects.appendChild(subjectBunkable);
+			attendanceElement.appendChild(subjects);
 		}
 	}
 
 	if(window.location.href == "https://www.rajagiritech.ac.in/stud/KTU/Parent/Leave.asp?code=2021S8IT") {
 		let attendanceTable = document.getElementsByTagName('table');
 		let leaveData = generateLeaveData(attendanceTable);
-		if(Object.keys(leaveData).length === 0) {
-			console.log("Full Attendance!");
-		} else {
-			let leaveCount = subjectwiseLeaveCount(leaveData);
-			calcAttendanceDetails(leaveCount);
-		}
 		
 		removeExistingTags();
+
+		document.getElementsByTagName('html')[0].append(head);
+
+		// To generate body tag and append its elements
+		let body = document.createElement("body");
+		body.insertAdjacentHTML('afterbegin','<div class="attendance"></div><div class="leave"></div>');
+		document.getElementsByTagName('html')[0].append(body);
+		
+		let leaveCount = subjectwiseLeaveCount(leaveData);
+		calcAttendanceDetails(leaveCount);
+
 	}
 	else if(window.location.href == "https://www.rajagiritech.ac.in/stud/KTU/Parent/Home.asp") {
 		removeExistingTags();
